159590f1eb4392d3c2e72ef7cd7dffc53acde6fb
vfarcic <viktor@farcic.com>
2015-09-24 14:52:12 +0200

    Fixing bugs

--- ansible/roles/service-bg/files/get-color.sh
+++ ansible/roles/service-bg/files/get-color.sh
@@ -5,10 +5,10 @@ SERVICE_NAME=$1
 CURR_COLOR=`curl \
     http://localhost:8500/v1/kv/$SERVICE_NAME/color?raw`
 
-if [ $CURR_COLOR == "green" ]; then
-    NEXT_COLOR="blue"
-else
+if [ $CURR_COLOR == "blue" ]; then
     NEXT_COLOR="green"
+else
+    NEXT_COLOR="blue"
 fi
 
 echo $NEXT_COLOR
