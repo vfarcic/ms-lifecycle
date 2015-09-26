066f51180ca0471be2960a42f12df181b93612bb
vfarcic <viktor@farcic.com>
2015-09-26 19:39:42 +0200

    Finished Swarm set up

--- scripts/preload_cd.sh
+++ scripts/preload_cd.sh
@@ -4,8 +4,8 @@ git clone https://github.com/vfarcic/books-ms.git
 docker pull vfarcic/books-ms-tests
 docker pull vfarcic/books-ms
 docker pull mongo
-docker pull gilderlabs/registrator
-docker tag gilderlabs/registrator 10.100.198.200:5000/registrator
+docker pull gliderlabs/registrator
+docker tag gliderlabs/registrator 10.100.198.200:5000/registrator
 docker push 10.100.198.200:5000/registrator
 docker pull nginx
 docker tag nginx 10.100.198.200:5000/nginx
--- scripts/preload_swarm.sh
+++ scripts/preload_swarm.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 docker pull swarm
+docker tag swarm 10.100.198.200:5000/swarm
+docker push 10.100.198.200:5000/swarm
