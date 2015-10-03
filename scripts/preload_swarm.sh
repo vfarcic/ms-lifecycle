4acc5b19afc2a6cfb4e054bc60020f814d88097d
vfarcic <viktor@farcic.com>
2015-10-03 15:09:40 +0200

    Refactoring

--- scripts/preload_swarm.sh
+++ scripts/preload_swarm.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 docker pull swarm
+
 docker pull nginx
+
 docker pull gliderlabs/registrator
--- scripts/preload_swarm_node.sh
+++ scripts/preload_swarm_node.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 docker pull mongo
+
 docker pull 10.100.198.200:5000/books-ms
