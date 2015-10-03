457cb5a6808331ea7d07b1699ff748e69928f863
vfarcic <viktor@farcic.com>
2015-10-04 00:15:28 +0200

    Started self-healing

--- ansible/roles/consul-healing/templates/manage_critical.sh
+++ ansible/roles/consul-healing/templates/manage_critical.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
-#RED='\033[0;31m'
-#NC='\033[0;0m'
+RED='\033[0;31m'
+NC='\033[0;0m'
 
 read -r JSON
-#echo "Consul watch request:"
+echo "Consul watch request:"
 echo "$JSON"
-#echo "$JSON" | jq -r '.[] | select(.CheckID | contains("service:")) | .ServiceName' | while read SERVICE_NAME
-#do
-#  echo ""
-#  echo -e ">>> ${RED}Service $SERVICE_NAME is critical${NC}"
-#  echo ""
-#  echo "Triggering Jenkins job http://{{ ip }}:8080/job/$SERVICE_NAME/build"
-#  curl -X POST http://{{ ip }}:8080/job/$SERVICE_NAME/build
-#done
+echo "$JSON" | jq -r '.[] | select(.CheckID | contains("service:")) | .ServiceName' | while read SERVICE_NAME
+do
+    echo ""
+    echo -e "${RED}>>> Service $SERVICE_NAME is critical${NC}"
+    echo ""
+    echo "Triggering Jenkins job http://{{ jenkins_ip }}:8080/job/service-redeploy/build"
+    curl -X POST http://{{ jenkins_ip }}:8080/job/service-redeploy/build \
+        --data-urlencode json="{\"parameter\": [{\"name\": \"SERVICE_NAME\", \"value\": \"$SERVICE_NAME\"}]}"
+    echo ""
+done
