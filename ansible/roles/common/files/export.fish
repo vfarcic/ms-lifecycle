abb24f38aa126eed8a8505e200265862be739d07
vfarcic <viktor@farcic.com>
2015-10-16 11:17:46 +0200

    Started HAProxy role

---
+++ ansible/roles/common/files/export.fish
@@ -0,0 +1,9 @@
+function export
+    if [ $argv ]
+        set var (echo $argv | cut -f1 -d=)
+        set val (echo $argv | cut -f2 -d=)
+        set -g -x $var $val
+    else
+        echo 'export var=value'
+    end
+end
