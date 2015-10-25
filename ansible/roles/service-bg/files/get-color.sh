56495a282ded416830a147219249b6f1c7bb253d
vfarcic <viktor@farcic.com>
2015-10-25 15:15:23 +0100

    Changed single to double quotes

--- ansible/roles/service-bg/files/get-color.sh
+++ ansible/roles/service-bg/files/get-color.sh
@@ -2,13 +2,11 @@
 
 SERVICE_NAME=$1
 
-CURR_COLOR=`curl \
-    http://localhost:8500/v1/kv/$SERVICE_NAME/color?raw`
+CURR_COLOR="curl \
+    http://localhost:8500/v1/kv/$SERVICE_NAME/color?raw"
 
 if [ $CURR_COLOR == "blue" ]; then
-    NEXT_COLOR="green"
+    echo "green"
 else
-    NEXT_COLOR="blue"
+    echo "blue"
 fi
-
-echo $NEXT_COLOR
