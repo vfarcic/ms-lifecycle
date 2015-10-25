01b9ca43834764aa24ed8e96a237c0a1ab9aac38
vfarcic <viktor@farcic.com>
2015-10-25 15:29:41 +0100

    Updated get_color.sh script

--- ansible/roles/service-bg/files/get-color.sh
+++ ansible/roles/service-bg/files/get-color.sh
@@ -2,10 +2,10 @@
 
 SERVICE_NAME=$1
 
-CURR_COLOR="curl \
-    http://localhost:8500/v1/kv/$SERVICE_NAME/color?raw"
+CURR_COLOR=`curl \
+    http://localhost:8500/v1/kv/$SERVICE_NAME/color?raw`
 
-if [ $CURR_COLOR == "blue" ]; then
+if [ "$CURR_COLOR" == "blue" ]; then
     echo "green"
 else
     echo "blue"
--- ansible/roles/service-swarm/files/get-color.sh
+++ ansible/roles/service-swarm/files/get-color.sh
@@ -5,10 +5,8 @@ SERVICE_NAME=$1
 CURR_COLOR=`curl \
     http://localhost:8500/v1/kv/$SERVICE_NAME/color?raw`
 
-if [ $CURR_COLOR == "blue" ]; then
-    NEXT_COLOR="green"
+if [ "$CURR_COLOR" == "blue" ]; then
+    echo "green"
 else
-    NEXT_COLOR="blue"
+    echo "blue"
 fi
-
-echo $NEXT_COLOR
