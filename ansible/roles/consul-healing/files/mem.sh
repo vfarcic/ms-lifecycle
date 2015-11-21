49ff9acdef125005cfda92923d06737a8582089c
vfarcic <viktor@farcic.com>
2015-11-21 12:38:27 +0100

    Refactoring

--- ansible/roles/consul-healing/files/mem.sh
+++ ansible/roles/consul-healing/files/mem.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-set -- $(free -m | awk 'NR==2{print $2" "$3}')
+set -- $(free -m | awk 'NR==2{print $2" "$3" "$7}')
 total=$1
 used=$2
-used_percent=$(( $used * 100 / $total ))
+cached=$3
+used_percent=$(( ($used - $cached) * 100 / $total ))
 printf "Memory Usage: %s/%sMB (%s%%)\n" $used $total $used_percent
 if [ $used_percent -gt 95 ]; then
   exit 2
