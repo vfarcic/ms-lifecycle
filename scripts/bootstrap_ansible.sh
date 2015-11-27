8c3b2d295dedd8711e16d37e2c66643890e9bbd7
vfarcic <viktor@farcic.com>
2015-11-27 15:26:33 +0100

    Removed Ansible human logs

--- scripts/bootstrap_ansible.sh
+++ scripts/bootstrap_ansible.sh
@@ -12,6 +12,6 @@ apt-get install -y python-pip python-dev
 pip install ansible==1.9.2
 mkdir -p /etc/ansible
 touch /etc/ansible/hosts
-cp /vagrant/ansible/ansible.cfg /etc/ansible/ansible.cfg
-mkdir -p /etc/ansible/callback_plugins/
-cp /vagrant/ansible/plugins/human_log.py /etc/ansible/callback_plugins/human_log.py
+#cp /vagrant/ansible/ansible.cfg /etc/ansible/ansible.cfg
+#mkdir -p /etc/ansible/callback_plugins/
+#cp /vagrant/ansible/plugins/human_log.py /etc/ansible/callback_plugins/human_log.py
