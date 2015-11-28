f78a4960b0d899dffa810c7a8a46abf882ad637f
vfarcic <viktor@farcic.com>
2015-11-28 20:41:42 +0100

    Added service-flow.groovy

--- jenkins/service-flow.groovy
+++ jenkins/service-flow.groovy
@@ -9,13 +9,13 @@ node("cd") {
     env.PYTHONUNBUFFERED = 1
 
     stage "> Provisioning"
-    if (provision.toBoolean()) {
+    if (new Boolean(provision)) {
         sh "ansible-playbook /vagrant/ansible/swarm.yml -i /vagrant/ansible/hosts/prod"
     }
 
     stage "> Pre-Deployment"
     git url: "https://github.com/vfarcic/${service}.git"
-    if (build.toBoolean()) {
+    if (new Boolean(build)) {
         sh "sudo docker build -t ${registry}:5000/${service}-tests -f Dockerfile.test ."
         sh "sudo docker-compose -f docker-compose-dev.yml run --rm tests"
         def app = docker.build "${registry}:5000/${service}"
@@ -25,7 +25,7 @@ node("cd") {
     stage "> Deployment"
     env.DOCKER_HOST = "tcp://${swarmMaster}:2375"
     def instances = getInstances(swarmMaster, service)
-    if (build.toBoolean()) {
+    if (new Boolean(build)) {
         sh "docker-compose -f docker-compose-swarm.yml pull app-${nextColor}"
     }
     sh "docker-compose -f docker-compose-swarm.yml --x-networking up -d db"
