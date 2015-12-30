948391d20064e1052b7314882400133434cc4a51
vfarcic <viktor@farcic.com>
2015-12-30 23:48:13 +0100

    Jenkins

---
+++ ansible/roles/jenkins/files/scripts/workflow-util.groovy
@@ -0,0 +1,51 @@
+def provision(playbook) {
+    stage "Provision"
+    env.PYTHONUNBUFFERED = 1
+    sh "ansible-playbook /vagrant/ansible/${playbook} \
+        -i /vagrant/ansible/hosts/prod"
+}
+
+def buildTests(serviceName, registryIpPort) {
+    stage "Build tests"
+    def tests = docker.image("${registryIpPort}/${serviceName}-tests")
+    tests.pull()
+    sh "docker build -t \"${registryIpPort}/${serviceName}-tests\" \
+        -f Dockerfile.test ."
+    tests.push()
+}
+
+def runTests(target, extraArgs) {
+    stage "Run ${target} tests"
+    sh "docker-compose -f docker-compose-dev.yml run --rm ${extraArgs} ${target}"
+}
+
+def buildService(serviceName, registryIpPort) {
+    stage "Build service"
+    def service = docker.image("${registryIpPort}/${serviceName}")
+    service.pull()
+    docker.build "${registryIpPort}/${serviceName}"
+    service.push()
+}
+
+def deploy(prodIp) {
+    stage "Deploy"
+    withEnv(["DOCKER_HOST=tcp://${prodIp}:2375"]) {
+        sh "docker-compose pull app"
+        sh "docker-compose up -d app"
+    }
+}
+
+def updateProxy(serviceName, proxyNode) {
+    stage "Update proxy"
+    stash includes: 'nginx-*', name: 'nginx'
+    node(proxyNode) {
+        unstash 'nginx'
+        sh "sudo cp nginx-includes.conf /data/nginx/includes/${serviceName}.conf"
+        sh "sudo consul-template \
+            -consul localhost:8500 \
+            -template \"nginx-upstreams.ctmpl:/data/nginx/upstreams/${serviceName}.conf:docker kill -s HUP nginx\" \
+            -once"
+    }
+}
+
+return this;
