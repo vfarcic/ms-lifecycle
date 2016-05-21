5d7b88cc78e1fd67f764c5b8b425ae1e7ca00810
vfarcic <viktor@farcic.com>
2016-05-22 00:39:07 +0200

    Added  --force-yes to Ansible in order to avoid the problem with the key signature

--- scripts/bootstrap_ansible.sh
+++ scripts/bootstrap_ansible.sh
@@ -6,7 +6,7 @@ echo "Installing Ansible..."
 apt-get install -y software-properties-common
 apt-add-repository ppa:ansible/ansible
 apt-get update
-apt-get install -y ansible
+apt-get install -y --force-yes ansible
 cp /vagrant/ansible/ansible.cfg /etc/ansible/ansible.cfg
 
 #apt-get update -y
